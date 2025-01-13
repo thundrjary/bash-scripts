df3abcc69af7ba5b1280709c4989a9697d1e8841
Jarom Later <jaromdl@gmail.com>
2025-01-12 17:21:05 -0700

    Using extended test command

--- file_growth_tracker.sh
+++ file_growth_tracker.sh
@@ -10,13 +10,13 @@ if [[ -z "${target_size}" || -z "${file_path}" ]]; then
 fi
 
 # Convert target size to bytes
-target_size_bytes=$(( target_size * 1024 * 1024 * 1024))
+target_size_bytes=$(( target_size * 1024 * 1024 * 1024 ))
 
 # Get current size of file in bytes
-current_size=$(du -b "${file_path}" | awk '{print $1}')
+current_size=$( du -b "${file_path}" | awk '{print $1}' )
 
 # Check if current size is greater than or equal to target size
-if [ "${current_size}" -ge "${target_size_bytes}" ]; then
+if [[ "${current_size}" -ge "${target_size_bytes}" ]]; then
   echo "File is already at or above target size."
   exit 1
 fi
@@ -25,20 +25,20 @@ fi
 remaining_size=$(( target_size_bytes - current_size ))
 
 # Get the start time and start size
-start_time=$(date +%s)
+start_time=$( date +%s )
 start_size="${current_size}"
 
 # Continuously update countdown
-while [ "${current_size}" -lt "${target_size_bytes}" ]
+while [[ "${current_size}" -lt "${target_size_bytes}" ]]
 do
-  current_size=$(du -b "${file_path}" | awk '{print $1}')
+  current_size=$( du -b "${file_path}" | awk '{print $1}' )
   remaining_size=$(( target_size_bytes - current_size ))
   
   # Calculate the growth rate in bytes/minute
-  current_time=$(date +%s)
+  current_time=$( date +%s )
   elapsed_time=$(( current_time - start_time ))
-  if [ "${elapsed_time}" -ne 0 ]; then
-    growth_rate=$((( current_size - start_size) / elapsed_time ))
+  if [[ "${elapsed_time}" -ne 0 ]]; then
+    growth_rate=$(( (current_size - start_size) / elapsed_time ))
 
     # Calculate how much longer it will take to reach the target size
     remaining_time=$(( remaining_size / growth_rate ))
